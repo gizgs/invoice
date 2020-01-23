efb268db38c00d1c7c4285424dce8adfbc8048b3
gizgs <arthur.giza@gmail.com>
2020-01-23 22:23:53 +0100

    Hello2

--- invoice_app/urls.py
+++ invoice_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.invoice_list, name='invoice_list'),
+	path('invoice/<int:pk>/', views.invoice_detail, name='invoice_detail'),
     #path('/a', views.post_list, name='post_list'),
 
 ]
--- invoice_app/views.py
+++ invoice_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Articles, Invoice
 
@@ -10,3 +10,8 @@ def post_list(request):
 def invoice_list(request):
 	invoice = Invoice.objects.order_by('invoice_nr')
 	return render(request, 'invoice_app/post_list.html', {'invoice' : invoice})
+
+def invoice_detail(request, pk):
+	#Invoice.objects.get(pk=pk)
+    invoice = get_object_or_404(Invoice, pk=pk)
+    return render(request, 'invoice_app/invoice_detail.html', {'invoice': invoice})
